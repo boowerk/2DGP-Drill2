ae3647ae8900f41b3a861887d8f615bf0fb22f79
boowerk <back20044@gmail.com>
2024-09-23 15:10:16 +0900

    Drill 2 제출 (2023184017 백지훈)

--- character_grass.py
+++ character_grass.py
@@ -6,48 +6,49 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 100
-y = 90
-while (x < 300):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    x += 2
-    delay(0.01)
+while (1):
+    x = 100
+    y = 90
+    while (x < 300):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
 
 
-while (y < 190):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    x -= 2
-    y += 2
-    delay(0.01)
-    
-while (x > 100):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    x -= 2
-    y -= 2
-    delay(0.01)
+    while (y < 190):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 2
+        y += 2
+        delay(0.01)
+        
+    while (x > 100):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 2
+        y -= 2
+        delay(0.01)
 
-center_x = 400
-center_y = 300
-radius = 200
+    center_x = 400
+    center_y = 300
+    radius = 200
 
-angle = 0
+    angle = 0
 
-while (angle < 360):
-    clear_canvas_now()
-    grass.draw_now(400, 30)    
-    
-    x = center_x + radius * math.cos(math.radians(angle))
-    y = center_y + radius * math.sin(math.radians(angle))
-    character.draw_now(x, y)
+    while (angle < 360):
+        clear_canvas_now()
+        grass.draw_now(400, 30)    
+        
+        x = center_x + radius * math.cos(math.radians(angle))
+        y = center_y + radius * math.sin(math.radians(angle))
+        character.draw_now(x, y)
 
-    angle += 1
-    delay(0.01)
+        angle += 1
+        delay(0.01)
 
 
 
