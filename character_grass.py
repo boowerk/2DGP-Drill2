aaa876d37e4a26968e5b3f347fbf5fd304a3ad17
boowerk <back20044@gmail.com>
2024-09-23 14:59:19 +0900

    삼각형 그리기 구현

--- character_grass.py
+++ character_grass.py
@@ -5,13 +5,30 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while (x < 800):
+x = 100
+y = 90
+while (x < 300):
     clear_canvas_now()
     grass.draw_now(400, 30)
-    character.draw_now(x, 90)
+    character.draw_now(x, y)
     x += 2
     delay(0.01)
 
-close_canvas()
+
+while (y < 190):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    x -= 2
+    y += 2
+    delay(0.01)
     
+while (x > 100):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    x -= 2
+    y -= 2
+    delay(0.01)
+
+close_canvas()
